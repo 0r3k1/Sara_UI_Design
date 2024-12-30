e71f29f429b1c48e46296eb9bbf58ec0305400e2
Oreki <45185337+0r3k1@users.noreply.github.com>
2024-12-30 01:02:56 -0600

    primera vercion de los controles personalizados
    primera vercion de los controles personalizados donde se inclulle button, comboBox, dataPiker, line, pictureBox, progressbar, radioButton, textBox y toggleButton

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Sara_UI_Design Controles personalizados")]
+[assembly: AssemblyDescription("Dale un toque profesional a tus aplicaciones de Windows Forms con esta biblioteca de controles personalizados. Incluye botones, cuadros de texto, imágenes y más, con estilos predefinidos para facilitar su uso")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cristobal Rodas")]
+[assembly: AssemblyProduct("Sara_UI_Design")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2024")]
+[assembly: AssemblyTrademark("Cristobal Enrique Rodas Arroyo")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("ca322e9e-3f7e-429e-9869-c7218fd2fd0f")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
